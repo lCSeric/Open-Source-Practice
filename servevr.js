c772cf7d0388802567f10f2a2b190ff99f0d062a
lCSeric <133724439+lCSeric@users.noreply.github.com>
2023-05-24 21:56:24 +0800

    servevr.js

--- servevr.js
+++ servevr.js
@@ -1,18 +1,62 @@
+const express = require('express');
 const mysql = require('mysql2');
 
-// 创建数据库连接
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON body
+app.use(express.json());
+
+// Create a database connection
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'your_username',
   password: 'your_password',
-  database: 'mydatabase' // 数据库名称
+  database: 'mydatabase' // Replace with your database name
 });
 
-// 测试连接
+// Test the database connection
 connection.connect((err) => {
   if (err) {
-    console.error('数据库连接失败：', err);
+    console.error('Database connection failed:', err);
     return;
   }
-  console.log('数据库连接成功！');
+  console.log('Database connection successful!');
+});
+
+// POST endpoint for credit calculation
+app.post('/calculate', (req, res) => {
+  // Get the course and credits from the request body
+  const { course, credits } = req.body;
+
+  // Perform credit calculation logic here
+  // ...
+
+  // Generate the response
+  const result = {
+    currentCredits: 100,  // Sample data, replace with actual calculations
+    remainingCredits: 50,
+    requiredCredits: 120,
+    departmentalElectives: 20,
+    nonDepartmentalElectives: 15,
+    generalEducation: 35
+  };
+
+  // Insert the calculation result into the database
+  const query = 'INSERT INTO credit_results SET ?';
+  connection.query(query, result, (err, insertResult) => {
+    if (err) {
+      console.error('Error inserting into database:', err);
+      res.sendStatus(500);
+      return;
+    }
+
+    // Send the response
+    res.json(result);
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
